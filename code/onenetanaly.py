b2b4a78d88b2260a0cd2b745886dfe2fa92bcea8
brokendragon <chenting19870201@163.com>
2018-04-04 15:20:14 +0800

    Update onenetanaly.py

--- code/onenetanaly.py
+++ code/onenetanaly.py
@@ -24,16 +24,12 @@ def corrcoef(y):
 	n=len(x)
 	#print x
 	#print y
-	#求和
 	sum1=sum(x)
 	sum2=sum(y)
-    #求乘积之和
 	sumofxy=multipl(x,y)
-    #求平方和
 	sumofx2 = sum([pow(i,2) for i in x])
 	sumofy2 = sum([pow(j,2) for j in y])
 	num=sumofxy-(float(sum1)*float(sum2)/n)
-    #计算皮尔逊相关系数
 	den=sqrt((sumofx2-float(sum1**2)/n)*(sumofy2-float(sum2**2)/n))
 	return num/den
 
